a7bc1855afc6b9600329b2b2293e107cfb4c1086
Xiao Hu <xiao@squareup.com>
2018-10-24 15:00:27 -0700

    update flutter example package name

--- example/android/app/src/main/java/com/example/flutter/squareup/sdk/reader/MainActivity.java
+++ example/android/app/src/main/java/com/example/flutter/squareup/sdk/reader/MainActivity.java
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package com.squareup.readersdkflutterpluginexample;
+package com.example.flutter.squareup.sdk.reader;
 
 import android.os.Bundle;
 import com.squareup.sdk.reader.ReaderSdk;
